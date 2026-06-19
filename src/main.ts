b91463df72e09b08ea53570529cbe75fa51b5088
Mojtaba Ahmadzadeh <mojtabaahmadzadeh.2024@gmail.com>
2026-06-19 16:09:32 +0330

    add project

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerConfigInit } from './config/swagger.config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  SwaggerConfigInit(app)
+  const port = configService.get<number>('PORT') ?? 3000;
+  const env = configService.get<string>('NODE_ENV') ?? 'development';
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(port);
+
+  console.log(`🚀 Server running on: http://localhost:${port}/swagger`);
+  console.log(`🌍 Environment: ${env.toUpperCase()}`);
+}
+
+bootstrap();
