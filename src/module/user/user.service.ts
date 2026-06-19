c20a176268012424583b19aa3e2f84ce167bd9ff
Mojtaba Ahmadzadeh <mojtabaahmadzadeh.2024@gmail.com>
2026-06-19 21:25:44 +0330

    feate(user): add api findAll & pagination

--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -1,26 +1,32 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Injectable } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UserEntity } from "./entities/user.entity";
+import { Repository } from "typeorm";
+import { PaginationDto } from "src/common/dto/pagination.dto";
+import { paginationGenerator, paginationSolver } from "src/common/utils/pagination.utils";
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
+  async findAll(paginationDto: PaginationDto) {
+    const { skip, limit, page } = paginationSolver(paginationDto);
 
-  findAll() {
-    return `This action returns all user`;
-  }
+    const totalCount = await this.userRepository.count();
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    const users = await this.userRepository.find({
+      skip,
+      take: limit,
+      order: { createdAt: "DESC" },
+    });
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    return {
+      pagination: paginationGenerator(totalCount, page, limit),
+      data: users,
+    };
   }
 }
